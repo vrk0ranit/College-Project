b551e9eb4d834529a9aa7ad29908dbd7f62ddb56
vrk0ranit <ranitbag49@gmail.com>
2025-11-09 17:17:02 +0530

    url2

--- backend/app.js
+++ backend/app.js
@@ -6,7 +6,7 @@ const bodyParser = require("body-parser");
 const cors = require("cors");
 
 app.use(cors({
-  origin: 'https://decorease-ranitbag.onrender.com/',
+  origin: 'https://decorease-ranitbag.onrender.com',
   credentials: true
 }));
 
